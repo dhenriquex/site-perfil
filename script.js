51b259a04642df3193f283bbbd0b3d69c10a7b93
Davi Henrique <133409016+dhenriquex@users.noreply.github.com>
2024-01-04 14:01:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode(){
+  const html = document.documentElement;
+  //if(html.classList.contains('light')){
+  //html.classList.remove('light');
+  //}else{
+  // html.classList.add('light');
+  //}
+  html.classList.toggle('light')
+  //para trocar a foto usama:
+  //const img=document.querySelector("seletor css")
+  
+}
